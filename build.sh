d95d8205eda6930a1e73a40907d3d17cf866efa7
ipj01 <isaiah.paulraj@sky.uk>
2022-11-28 16:49:22 +0000

    RDK-38603: build.sh supported with utcore clone and build_ut.sh file is added

--- build.sh
+++ build.sh
@@ -1,23 +1,40 @@
 #!/usr/bin/env bash
 
 # Clone the Unit Test Requirements
-TEST_REPO=git@github.com:comcast-sky/rdk-components-ut-core.git
+#TEST_REPO=git@github.com:comcast-sky/rdk-components-ut-core.git
+TEST_REPO="git://github.com/comcast-sky/rdk-components-ut-core.git"
+
+#SCRIPTDIR=${BASH_SOURCE[0]%/*} #$(pwd) #get data from running script file.
+SCRIPTDIR="$(dirname "$0")"
+
+UT_CORE_PATH=${SCRIPTDIR}/ut-core
 
 # Set default UT_PROJECT_VERSION to master
 if [ -z "${UT_PROJECT_VERSION}" ]; then
     UT_PROJECT_VERSION=master
 fi
-
 # Check if the common document configuration is present, if not clone it
-if [ -d "./ut-core" ]; then
-    echo "compiling ut-core from ${PWD}"
-    make -C . -f Makefile $@
+if [ -d "${UT_CORE_PATH}" ]; then
+    echo "executing UT-core makefile"
+    pushd ${SCRIPTDIR} > /dev/null      
+    make -C . -f Makefile $@ 
+    popd > /dev/null       
 else
-    echo "Cloning Unit Test Core System"
+    echo "Cloning Unit Test Core System - Path "
+    pushd "${SCRIPTDIR}" > /dev/null  
     git clone ${TEST_REPO} ut-core
-    cd ./ut-core
-    git checkout ${UT_PROJECT_VERSION}
-    ./build.sh
-    cd ..
-    ./${0} $@
+    popd > /dev/null    
+    pushd "${UT_CORE_PATH}" > /dev/null   
+    echo "${UT_CORE_PATH}"    
+    git checkout ${UT_PROJECT_VERSION} 
+    popd > /dev/null   
+    if [ -d "${UT_CORE_PATH}" ]; then
+        pushd "${UT_CORE_PATH}" > /dev/null
+        ./build.sh  #Clone utcore framework
+        popd > /dev/null
+        pushd ${SCRIPTDIR} > /dev/null 
+        make -C . -f Makefile $@
+        popd > /dev/null 
+    fi   
+    ##./${0} $@ # Commented for testing purpose
 fi
