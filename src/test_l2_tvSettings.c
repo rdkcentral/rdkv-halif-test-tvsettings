99539dd14a2c6bdfe27f1c318acd632dea0f3155
Kumar, Ranjith (Software Engineer - Sky - Guest) <ranjith.kumar@sky.uk>
2024-02-12 18:58:58 +0530

    Update test_l2_tvSettings.c
    Signed-off-by: Kumar, Ranjith (Software Engineer - Sky - Guest) <ranjith.kumar@sky.uk>

--- src/test_l2_tvSettings.c
+++ src/test_l2_tvSettings.c
@@ -51,7 +51,7 @@
 */
 void test_l2_tvSettings (void)
 {
-	UT_FAIL(This function needs to be implemented!); 
+	UT_FAIL("This function needs to be implemented!"); 
 }
 
 static UT_test_suite_t * pSuite = NULL;
