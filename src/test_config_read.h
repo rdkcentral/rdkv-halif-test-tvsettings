6bc3edf7d6031ebab8cf95c59b2282eaa781d2ce
Kumar, Ranjith (Software Engineer - Sky - Guest) <ranjith.kumar@sky.uk>
2024-02-15 21:55:29 +0530

    Update test_config_read.h
    header file update
    
    Signed-off-by: Kumar, Ranjith (Software Engineer - Sky - Guest) <ranjith.kumar@sky.uk>

--- src/test_config_read.h
+++ src/test_config_read.h
@@ -1,3 +1,65 @@
+/**
+ *  If not stated otherwise in this file or this component's LICENSE
+ *  file the following copyright and licenses apply:
+ *
+ *  Copyright 2023 RDK Management
+ *
+ *  Licensed under the Apache License, Version 2.0 (the License);
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an AS IS BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+/**
+ * @addtogroup HPK Hardware Porting Kit
+ * @{
+ * @par The Hardware Porting Kit
+ * HPK is the next evolution of the well-defined Hardware Abstraction Layer
+ * (HAL), but augmented with more comprehensive documentation and test suites
+ * that OEM or SOC vendors can use to self-certify their ports before taking
+ * them to RDKM for validation or to an operator for final integration and
+ * deployment. The Hardware Porting Kit effectively enables an OEM and/or SOC
+ * vendor to self-certify their own Video Accelerator devices, with minimal RDKM
+ * assistance.
+ *
+ */
+
+/**
+ * @addtogroup TV_Settings TV Settings Module
+ * @{
+ */
+
+/**
+* @addtogroup TV_Settings_HAL TV Settings HAL
+* @{
+*/
+
+/**
+* @defgroup TV_Settings_HALTEST_L1 TV Settings HAL L1 Test Cases
+* @{
+*/
+
+/**
+ * @file test_config_read.h
+ * @page TV_Settings_HALTEST_L1  
+ *
+ * ## Module's Role
+ * This module includes Level 1 functional tests (success and failure scenarios).
+ * This is to ensure that the API meets the operational requirements of the TV SETTINGS across all vendors.
+ *
+ * **Pre-Conditions:**  None@n
+ * **Dependencies:** None@n
+ *
+ * Ref to API Definition specification documentation : [halSpec.md](../../../docs/halSpec.md)
+ */
+
 #ifndef TEST_CONFIG_READ_H
 #define TEST_CONFIG_READ_H
 
@@ -201,3 +263,8 @@ struct TvSettingConfig {
 int config_read(char *filename);
 
 #endif /*TEST_CONFIG_READ_H*/
+
+/** @} */ // End of TV_Settings_HALTEST_L1
+/** @} */ // End of TV_Settings_HAL
+/** @} */ // End of TV_Settings
+/** @} */ // End of HPK
