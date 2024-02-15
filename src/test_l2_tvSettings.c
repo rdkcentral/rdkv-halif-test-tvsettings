ec942f997794a00787906255d69b8ec54ef990bc
Kumar, Ranjith (Software Engineer - Sky - Guest) <ranjith.kumar@sky.uk>
2024-02-15 22:16:22 +0530

    Update test_l2_tvSettings.c
    updated l2
    
    Signed-off-by: Kumar, Ranjith (Software Engineer - Sky - Guest) <ranjith.kumar@sky.uk>

--- src/test_l2_tvSettings.c
+++ src/test_l2_tvSettings.c
@@ -18,19 +18,52 @@
 */
 
 /**
-* @file TODO: test_l2_tvSettings.c
-* @page module_name TODO: Required field, name of the main module
-* @subpage sub_page_name TODO: Add a function group if relevant
-*
-* ## Module's Role
-* TODO: Explain the module's role in the system in general
-* This is to ensure that the API meets the operational requirements of the module across all vendors.
-*
-* **Pre-Conditions:**  TODO: Add pre-conditions if any@n
-* **Dependencies:** TODO: Add dependencies if any@n
-*
-* Ref to API Definition specification documentation : [halSpec.md](../../../docs/halSpec.md)
+ * @addtogroup HPK Hardware Porting Kit
+ * @{
+ * @par The Hardware Porting Kit
+ * HPK is the next evolution of the well-defined Hardware Abstraction Layer
+ * (HAL), but augmented with more comprehensive documentation and test suites
+ * that OEM or SOC vendors can use to self-certify their ports before taking
+ * them to RDKM for validation or to an operator for final integration and
+ * deployment. The Hardware Porting Kit effectively enables an OEM and/or SOC
+ * vendor to self-certify their own Video Accelerator devices, with minimal RDKM
+ * assistance.
+ *
+ */
+
+/**
+ * @addtogroup TV_Settings TV Settings Module
+ * @par Application API Specification
+ * Described herein are the TV Settings HAL types and functions that are part of
+ * the Host subsystem. The Host subsystem manages system-specific HAL operations.
+ * @{
+ */
+
+/**
+* @addtogroup TV_Settings_HAL TV Settings HALTEST
+* @par Application API Specification
+* TvSettings HAL provides an interface for managing the TV settings for the TV settings module
+* @{
 */
+
+/**
+* @defgroup TV_Settings_HALTEST_L2 TV Settings HAL L1 Test Cases
+* @{
+*/
+
+/**
+ * @file test_l2_tvSettings.c
+ * @page TV_Settings_HALTEST_L2  
+ *
+ * ## Module's Role
+ * This module includes Level 2 functional tests (success and failure scenarios).
+ * This is to ensure that the API meets the operational requirements of the TV SETTINGS across all vendors.
+ *
+ * **Pre-Conditions:**  None@n
+ * **Dependencies:** None@n
+ *
+ * Ref to API Definition specification documentation : [tv-settings_halSpec.md](../../../docs/tv-settings_halSpec.md)
+ */
 
 #include <string.h>
 #include <stdlib.h>
@@ -75,3 +108,7 @@ int test_l2_tvSettings_register ( void )
 
 	return 0;
 } 
+/** @} */ // End of TV_Settings_HALTEST_L2
+/** @} */ // End of TV_Settings_HAL
+/** @} */ // End of TV_Settings
+/** @} */ // End of HPK
