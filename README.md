d163675d2e869e68e0007c917455ff7b64c27505
hari22yuva <56021398+hari22yuva@users.noreply.github.com>
2024-02-16 14:09:12 +0000

    baseline version

---
+++ README.md
@@ -0,0 +1,28 @@
+# Unit Testing Suite For TV Settings HAL
+
+## Table of Contents
+
+- [Acronyms, Terms and Abbreviations](#acronyms-terms-and-abbreviations)
+- [Description](#description)
+- [Reference Documents](#reference-documents)
+- [Notes](#notes)
+
+## Acronyms, Terms and Abbreviations
+
+- `L1` - Functional Tests
+- `HAL`- Hardware Abstraction Layer
+
+## Description
+
+This repository contains the Unit Test Suites L1 for TV Settings `HAL`.
+
+## Reference Documents
+
+|SNo|Document Name|Document Description|Document Link|
+|---|-------------|--------------------|-------------|
+|1|`HAL` Specification Document|This document provides specific information on the APIs for which tests are written in this module|[tv-settings_halSpec](https://github.com/rdkcentral/rdkv-halif-tvsettings/blob/main/docs/pages/tv-settings_halSpec.md "tv-settings_halSpec" )|
+|2|`L1` Tests | `L1` Test Case File for this module | [test_l1_tvSettings.c]( https://github.com/rdkcentral/rdkv-halif-test-tvsettings/blob/main/src/test_l1_tvSettings.c         "test_l1_tvSettings.c" )|
+
+## Notes
+
+- To run the binary, ensure that you pass the configuration file as an argument. The following example demonstrates how to do this:  ./hal_test pq_capablities.ini
---
+++ build.sh
@@ -0,0 +1,66 @@
+#!/usr/bin/env bash
+
+# *
+# * If not stated otherwise in this file or this component's LICENSE file the
+# * following copyright and licenses apply:
+# *
+# * Copyright 2023 RDK Management
+# *
+# * Licensed under the Apache License, Version 2.0 (the "License");
+# * you may not use this file except in compliance with the License.
+# * You may obtain a copy of the License at
+# *
+# * http://www.apache.org/licenses/LICENSE-2.0
+# *
+# * Unless required by applicable law or agreed to in writing, software
+# * distributed under the License is distributed on an "AS IS" BASIS,
+# * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# * See the License for the specific language governing permissions and
+# * limitations under the License.
+# *
+
+RED="\e[0;31m"
+YELLOW="\e[1;33m"
+GREEN="\e[0;32m"
+NC="\e[39m"
+
+# The purpose of this script is on a clean environement, is to give you the latest binary compatible version for testing
+# When the major version changes in the ut-core, what that signals is that the testings will have to be upgraded to support that version
+# Therefore in that case it warns you but doesnt' chnage to that version, which could cause your tests to break.
+# Change this to upgrade your UT-Core Major versions. Non ABI Changes 1.x.x are supported, between major revisions
+UT_PROJECT_MAJOR_VERSION="2."
+
+# Clone the Unit Test Requirements
+TEST_REPO=git@github.com:rdkcentral/ut-core.git
+
+# This function checks the latest version of UT core and recommends an upgrade if reuqired
+function check_next_revision()
+{
+    pushd ./ut-core
+    # Set default UT_CORE_PROJECT_VERSION to next revision, if it's set then we don't need to tell you again
+    if [ -v ${UT_CORE_PROJECT_VERSION} ]; then
+        UT_CORE_PROJECT_VERSION=$(git tag | grep ${UT_PROJECT_MAJOR_VERSION} | sort -r | head -n1)
+        UT_NEXT_VERSION=$(git tag | sort -r | head -n1)
+        echo -e ${YELLOW}ut-core version selected:[${UT_CORE_PROJECT_VERSION}]${NC}
+        if [ "${UT_NEXT_VERSION}" != "${UT_CORE_PROJECT_VERSION}" ]; then
+            echo -e ${RED}--- New Version of ut-core released [${UT_NEXT_VERSION}] consider upgrading ---${NC}
+        fi
+    fi
+    popd
+}
+
+# Check if the common document configuration is present, if not clone it
+if [ -d "./ut-core" ]; then
+    # ut-core exists so run the makefile from ut
+    check_next_revision
+    make -C . -f Makefile $@
+else
+    echo "Cloning Unit Test Core System"
+    git clone ${TEST_REPO} ut-core
+    check_next_revision
+    cd ./ut-core
+    git checkout ${UT_CORE_PROJECT_VERSION}
+    ./build.sh
+    cd ..
+    ./${0} $@
+fi
