f57e033e15ab2b2274033d5b97d14391f2a3cf8a
KarthikeyanR470 <179679359+KarthikeyanR470@users.noreply.github.com>
2025-05-09 11:51:13 +0100

    gh #103 removed L1_L2_TestCases/tvSettings dir

--- host/tests/L1_L2_TestCases/tvSettings_L1_L2_tests.py
+++ host/tests/L1_L2_TestCases/tvSettings_L1_L2_tests.py
@@ -25,7 +25,7 @@ import os
 import sys
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
-sys.path.append(os.path.join(dir_path, "../../"))
+sys.path.append(os.path.join(dir_path, "../"))
 
 from raft.framework.plugins.ut_raft.configRead import ConfigRead
 from raft.framework.core.logModule import logModule
